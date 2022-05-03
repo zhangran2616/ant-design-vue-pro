01190eb63b91ef28570c0f710c3b78c382290d8d
zhangran <371282726@qq.com>
2022-05-03 16:27:38 +0800

    注释固定路由方式返回的权限列表-弃用

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -62,17 +62,25 @@ const user = {
         getInfo().then(response => {
           const result = response.data
 
-          if (result.role && result.role.permissions.length > 0) {
+          // if (result.role && result.role.permissions.length > 0) {
+          //   const role = result.role
+          //   role.permissions = result.role.permissions
+          //   role.permissions.map(per => {
+          //     if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
+          //       const action = per.actionEntitySet.map(action => { return action.action })
+          //       per.actionList = action
+          //     }
+          //   })
+          //   role.permissionList = role.permissions.map(permission => { return permission.permissionId })
+          //   commit('SET_ROLES', result.role)
+          //   commit('SET_INFO', result)
+          // } else {
+          //   reject(new Error('getInfo: roles must be a non-null array !'))
+          // }
+
+          if (result.role) {
             const role = result.role
-            role.permissions = result.role.permissions
-            role.permissions.map(per => {
-              if (per.actionEntitySet != null && per.actionEntitySet.length > 0) {
-                const action = per.actionEntitySet.map(action => { return action.action })
-                per.actionList = action
-              }
-            })
-            role.permissionList = role.permissions.map(permission => { return permission.permissionId })
-            commit('SET_ROLES', result.role)
+            commit('SET_ROLES', role)
             commit('SET_INFO', result)
           } else {
             reject(new Error('getInfo: roles must be a non-null array !'))
