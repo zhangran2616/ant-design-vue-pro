cdc65173cca207ea94fc7bb8b849e97685c57a5b
zhangran <371282726@qq.com>
2022-05-19 16:19:21 +0800

    子网

--- src/views/network/modules/CreateForm.vue
+++ src/views/network/modules/CreateForm.vue
@@ -15,17 +15,17 @@
           <a-input v-decorator="['id', { initialValue: 0 }]" disabled />
         </a-form-item>
         <a-form-item label="云平台">
-          <a-select @change="getDc" placeholder="请选择" v-decorator="['cpfId', {rules: [{required: true, message: '请选择云平台'}]}]">
+          <a-select :disabled=" model && model.id > 0 " @change="getDc" placeholder="请选择" v-decorator="['cpfId', {rules: [{required: true, message: '请选择云平台'}]}]">
             <a-select-option v-for="(item, index) in platforms" :value="item.id" :key="index">{{ item.name }}</a-select-option>
           </a-select>
         </a-form-item>
         <a-form-item label="数据中心">
-          <a-select @change="getNetworkLabel" placeholder="请选择" v-decorator="['datacenter', {rules: [{required: true, message: '请选择数据中心'}]}]">
+          <a-select :disabled=" model && model.id > 0 " @change="getNetworkLabel" placeholder="请选择" v-decorator="['datacenter', {rules: [{required: true, message: '请选择数据中心'}]}]">
             <a-select-option v-for="(item, index) in datacenters" :value="item.uuid" :key="index">{{ item.name }}</a-select-option>
           </a-select>
         </a-form-item>
         <a-form-item label="网络标签">
-          <a-select placeholder="请选择" v-decorator="['networkLabel', {rules: [{required: true, message: '请选择网络标签'}]}]">
+          <a-select :disabled=" model && model.id > 0 " placeholder="请选择" v-decorator="['networkLabel', {rules: [{required: true, message: '请选择网络标签'}]}]">
             <a-select-option v-for="(item, index) in networkLabels" :value="item.uuid" :key="index">{{ item.name }}</a-select-option>
           </a-select>
         </a-form-item>
@@ -33,7 +33,7 @@
           <a-input v-decorator="['name', {rules: [{required: true, min: 1, max: 200, message: '请输入名称长度1-200个字符！'}]}]" />
         </a-form-item>
         <a-form-item label="IP范围">
-          <a-input v-decorator="['ipPool', {rules: [{required: true, min: 1,max: 200, message: '请输入IP范围长度1-200个字符！'}]}]" />
+          <a-input :disabled=" model && model.id > 0 " v-decorator="['ipPool', {rules: [{required: true, min: 1,max: 200, message: '请输入IP范围长度1-200个字符！'}]}]" />
         </a-form-item>
         <a-form-item label="网关">
           <a-input v-decorator="['gateway', {rules: [{required: true, min: 1,max: 20, message: '请输入网关长度1-20个字符！'}]}]" />
@@ -113,6 +113,15 @@ export default {
   },
   methods: {
     getDc (cpfId) {
+      this.networkLabels = []
+      this.datacenters = []
+      if (!cpfId) {
+        return
+      }
+      this.form.setFieldsValue({
+        datacenter: undefined,
+        networkLabel: undefined
+      })
       const requestParameters = { 'pageSize': -1, 'cpfId': cpfId }
       queryDc(requestParameters)
       .then(res => {
@@ -120,7 +129,14 @@ export default {
       })
     },
     getNetworkLabel (uuid) {
-      const requestParameters = { 'pageSize': -1, 'uuid': uuid }
+       if (!uuid) {
+        return
+      }
+      this.form.setFieldsValue({
+        networkLabel: undefined
+      })
+      const cpfId = this.form.getFieldValue('cpfId')
+      const requestParameters = { 'pageSize': -1, 'cpfId': cpfId, 'uuid': uuid }
       queryNetworkLabel(requestParameters)
       .then(res => {
        this.networkLabels = res.data.records
